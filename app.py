4056cd29edf4463b9b1c8a8174a3bf41a9031c7b
Brice Fernandes <bfer@cbnl.com>
2012-02-14 18:19:09 +0000

    added templates, changed structure

--- app.py
+++ app.py
@@ -3,11 +3,14 @@
 import sys
 sys.path.append("./modules/")
 
+import os
+import ConfigParser
+from functools import *
+
 import pystache
 import bottle
 
-from bottle import route, run, request, response, get, post, redirect, abort
-
+from bottle import route, run, request, response, get, post, redirect, abort, static_file, error
 
 #
 # Configuration
@@ -23,13 +26,54 @@ def renderfile(name, data=None):
                 os.path.isfile,
                 map(
                   partial(os.path.join, config.get("app", "templatepath")),
-                  ["%S.mustache"%name, "%s.html"%name, name]))
+                  ["%s.mustache"%name, "%s.html"%name, name]))
   for name in fnames:
-    return pystache.render(open(name, "r").read, data, path=config.get("app", "templatepath"))
+    return pystache.render(open(name, "r").read(), data, path=config.get("app", "templatepath"))
+
+
+
+
+def show_flowDiagram():
+  return "<h3>flow diagram placeholder</h3>"
+
+
+
+pages = {
+  "overview" : {
+    "name" : "Overview", 
+    "href" : "/overview",
+    "template" : "overview", 
+    "tabs" : [
+      { "name" : "Flow Diagram",
+        "content" : show_flowDiagram }
+    ]
+  },
+}
+
+def genpage(name):
+  if name in pages:
+    return renderfile(pages[name]["template"])
+  else:
+    abort(404, "This page does not exist")
+
 
 @route("/")
+@route("/home")
 def home():
-  return "Home Sweet Home!"
+  return renderfile("home")
 
+@route("/<name>")
+def home(name):
+  return genpage(name) 
+
+
+@route("/static/<filepath:path>")
+def static(filepath):
+  return static_file(filepath, root="./static/")
+
+
+@error(404)
+def error404(error):
+  return renderfile("404")
 
 run(host="0.0.0.0", port="8009", debug=True, reloader=True)
