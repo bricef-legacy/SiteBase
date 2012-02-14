fa79c4095068b9a241b070a3d848a58e50046ecc
Brice Fernandes <bfer@cbnl.com>
2012-02-14 16:28:37 +0000

    added mustache and evolved app

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 
-from bottle import route, run, request, response, get, post 
 
+import sys
+sys.path.append("./modules/")
+
+import pystache
+import bottle
+
+from bottle import route, run, request, response, get, post, redirect, abort
+
+
+#
+# Configuration
+#
+config = ConfigParser.SafeConfigParser()
+config.read("config.ini")
+
+bottle.debug(True)
+
+def renderfile(name, data=None):
+  #This would be more readable in lisp...Coulda shoulda woulda
+  fnames = filter(
+                os.path.isfile,
+                map(
+                  partial(os.path.join, config.get("app", "templatepath")),
+                  ["%S.mustache"%name, "%s.html"%name, name]))
+  for name in fnames:
+    return pystache.render(open(name, "r").read, data, path=config.get("app", "templatepath"))
 
 @route("/")
 def home():
